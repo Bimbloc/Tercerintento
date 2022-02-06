c1331550cb94397447ea53886023af86efb00c0a
laurma40 <98830222+laurma40@users.noreply.github.com>
2022-02-06 12:43:31 +0100

    CUBO MOVEL

--- Assets/Scripts/lauracubo.cs
+++ Assets/Scripts/lauracubo.cs
@@ -5,9 +5,59 @@ using UnityEngine;
 public class lauracubo : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody rb;
+    // INPUT
+    float vx = 0;
+    float vy = 0;
+    float dz = 0;
+    bool vz = false;
+    new Vector3 direc;
+    [SerializeField] // Permite modificar en Unity
+    float velocidad = 34;
+    // Sólo usar int en interfaces, el resto float
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        vx = Input.GetAxis("Horizontal"); // Q eje
+        vy = Input.GetAxis("Vertical"); // Devuelve un número
+        // Una forma
+        // direc = new Vector3(vx, vy, 0); // Es un vector
+        // Otra forma mejor
+        direc.x = vx;
+        direc.y = vy;
+    }
+    void FixedUpdate()
+    { //x,y,z estamos en un espacio tridimensional
+        vx = Input.GetAxis("Horizontal");
+        vy = Input.GetAxis("Vertical");
+        vz = Input.GetButton("Jump"); // Devuelve si está pulsado o no
+        if (vz)
+        {
+            dz = 1;
+        }
+        else
+            dz = 0;
+
+        direc.x = vx;
+        direc.y = vy;
+        direc.z = dz;
+        rb.velocity = direc * velocidad;
+        Debug.Log(vz);
+        /*if (vx > -0.001 && vx < 0.001) // Esto es por los gamepads q no funcionan igual q los teclados cuando se ha parado de presionar
+        {
+            // Otra forma para q no frene de golpe
+            if (rb.velocity.x > 0.5)
+                rb.velocity = rb.velocity - new Vector3(0.5f, 0, 0);
+            else if (rb.velocity.x < -0.5)
+            {
+                rb.velocity = rb.velocity + new Vector3(0.5f, 0, 0);
+            }
+            else
+                rb.velocity = new Vector3(0, 0, 0);
+        }
+        else
+            rb.AddForce(new Vector3(1, 0, 0) * vx * velocidad);
+        */
     }
 
     // Update is called once per frame
