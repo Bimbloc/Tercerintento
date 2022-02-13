bd6ed3fa06174a3dbbb91bb0978e5a97908cf5c0
javics2002 <49459590+javics2002@users.noreply.github.com>
2022-02-13 11:41:40 +0100

    Sonidos

--- Assets/Scripts/AbrirCerrar.cs
+++ Assets/Scripts/AbrirCerrar.cs
@@ -6,18 +6,28 @@ public class AbrirCerrar : MonoBehaviour
 {
     private Animator anim;
 
+    [SerializeField]
+    AudioClip abrir, cerrar;
+
+    AudioSource audioSource;
+
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     public void Abrir()
     {
         anim.SetBool("Abierto", true);
+        audioSource.clip = abrir;
+        audioSource.Play();
     }
 
     public void Cerrar()
     {
         anim.SetBool("Abierto", false);
+        audioSource.clip = cerrar;
+        audioSource.Play();
     }
 }
