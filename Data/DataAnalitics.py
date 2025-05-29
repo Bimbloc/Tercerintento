eaa5ff58e2b2543d824edf07b5b7da6cac53fb14
Jjianuo <jwen@ucm.es>
2025-05-29 20:28:35 +0200

    Tablas en imagenes
    Co-Authored-By: Rocio Sanchez <82374311+Bimbloc@users.noreply.github.com>

--- Data/DataAnalitics.py
+++ Data/DataAnalitics.py
@@ -1,48 +1,47 @@
 import json
 import os
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 ##  Leer todos los archivos
-folder_path = './data'
+folder_path = os.path.dirname(os.path.realpath(__file__))
 
 #Pregunta 1 
 #Puntuaciones del libro ordebadas cronologicamente 
 orderRatings = np.zeros(6)
-#pregnta 2
 
+#pregunta 2
 #lista ordenada cronoligamente de decisiones correctas o erroneas
 playerChoices =[] #[Y,N,Y,Y,N]
-#jugement 1 == cielo 
+#jugement 1 == cielo
 #sinner 1 == cielo
 #jugement != sinner es un acierto
 correctGuessFavor = np.zeros(30) ##30 enteros , contamos cuantos acierto s Hay asociados a cada favor
-correctGuessSin =np.zeros(30)#30 enteros , contamos cuantos aciertos Hay asociados a cada pecado
-wrongGuessFavors =np.zeros(30)#30 enteros , contamos cuantos fallos Hay asociados a cada favor
-wrongGuessSins =np.zeros(30)##30 enteros , contamos cuantos fallos Hay asociados a cada pecado
+correctGuessSin = np.zeros(30) #30 enteros , contamos cuantos aciertos Hay asociados a cada pecado
+wrongGuessFavors = np.zeros(30) #30 enteros , contamos cuantos fallos Hay asociados a cada favor
+wrongGuessSins = np.zeros(30) #30 enteros , contamos cuantos fallos Hay asociados a cada pecado
 
 #pregunta 3
-dayLoses = np.zeros(6)#Contamos cuantas veces pierde en cada dia 
+dayLoses = np.zeros(6) #Contamos cuantas veces pierde en cada dia 
 lastDay = 0
-#pregunta 4 
 
-averageChoiceTime=[]#Tiempos medios de decision de cada dia ordenados cronologicamente
+#pregunta 4 
+averageChoiceTime=[] #Tiempos medios de decision de cada dia ordenados cronologicamente
 averageChoiceTime = np.zeros(6)
 
 #pregunta 5
-
-characterSentences = [np.zeros(6),np.zeros(6),np.zeros(6),np.zeros(6)]#Frases de cada tio de personaje 
+characterSentences = [np.zeros(6),np.zeros(6),np.zeros(6),np.zeros(6)] #Frases de cada tio de personaje 
 characterTypes = np.zeros(4)
+
 #pregunta 6
 #Sumamos apariciones en aciertos y en fallos 
 totalFavorApearance = np.zeros(30)
 totalSinAppearance = np.zeros(30)
     
-for file_name in os.listdir(folder_path):
+for file_name in os.listdir(folder_path + '/TelemetryData'):
         
-        if file_name.endswith('.json'):  # Process only JSON files
-            file_path = os.path.join(folder_path, file_name)
+        if file_name.endswith('.json'):  #Process only JSON files
+            file_path = os.path.join(folder_path + '/TelemetryData', file_name)
 
             print(f"Processing file: {file_name}")
 
@@ -86,7 +85,7 @@ plt.xlabel("Día")
 plt.ylabel("Nivel de orden")
 colors = [{t<=orderRatings.min()*1.15: 'red',orderRatings.min()*1.15 <t<=orderRatings.max()/1.25: 'orange', t>orderRatings.max()/1.25: 'green'}[True] for t in orderRatings ]
 plt.bar(days,orderRatings,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta1NivelesOrden.png")
 
 numvars = np.arange(0,30)
 plt.title("Pecados Aciertos")
@@ -95,28 +94,31 @@ plt.ylabel("Número de aciertos")
 colors = [{t<=correctGuessSin.min()*1.15: 'red',correctGuessSin.min()*1.15 <t<=correctGuessSin.max()/1.25: 'orange', t>correctGuessSin.max()/1.25: 'green'}[True] for t in correctGuessSin ]
 plt.xticks(numvars)
 plt.bar(numvars,correctGuessSin,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta2PecadosAciertos.png")
+
 plt.title("Pecados Fallos")
 plt.xlabel("ID del pecado")
 plt.ylabel("Número de fallos")
 colors = [{t<=wrongGuessSins.min()*1.15: 'red',wrongGuessSins.min()*1.15 <t<=wrongGuessSins.max()/1.25: 'orange', t>wrongGuessSins.max()/1.25: 'green'}[True] for t in wrongGuessSins ]
 plt.xticks(numvars)
 plt.bar(numvars,wrongGuessSins,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta2PecadosFallos.png")
+
 colors = [{t<=correctGuessFavor.min()*1.15: 'red',correctGuessFavor.min()*1.15 <t<=correctGuessFavor.max()/1.25: 'orange', t>correctGuessFavor.max()/1.25: 'green'}[True] for t in correctGuessFavor ]
 plt.title("Favores Aciertos")
 plt.xlabel("ID del favor")
 plt.ylabel("Número de Aciertos")
 plt.xticks(numvars)
 plt.bar(numvars,correctGuessFavor,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta2FavoresAciertos.png")
+
 colors = [{t<=wrongGuessFavors.min()*1.15: 'red',wrongGuessFavors.min()*1.15 <t<=wrongGuessFavors.max()/1.25: 'orange', t>wrongGuessFavors.max()/1.25: 'green'}[True] for t in wrongGuessFavors ]
 plt.title("Favores Fallos")
 plt.xlabel("ID del favor")
 plt.ylabel("Número de fallos")
 plt.xticks(numvars)
 plt.bar(numvars,wrongGuessFavors,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta2FavoresFallos.png")
 
 colors = [{t<=dayLoses.min()*1.15: 'red',dayLoses.min()*1.15 <t<=dayLoses.max()/1.25: 'orange', t>dayLoses.max()/1.25: 'green'}[True] for t in dayLoses ]
 plt.title("Derrotas por dia")
@@ -124,7 +126,7 @@ plt.xlabel("Día")
 plt.ylabel("Número de derrotas")
 plt.xticks(np.arange( 1,len(dayLoses)+1))
 plt.bar(np.arange(1,len(dayLoses)+1),dayLoses,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta3Derrotas.png")
 
 plt.title("Tiempo por dia")
 colors = [{t<=averageChoiceTime.min()*1.15: 'red',averageChoiceTime.min()*1.15 <t<=averageChoiceTime.max()/1.25: 'orange', t>averageChoiceTime.max()/1.25: 'green'}[True] for t in averageChoiceTime ]
@@ -134,7 +136,7 @@ plt.ylabel("Tiempo medio de decisión (ms)")
 plt.ylim(averageChoiceTime.min()/1.5,averageChoiceTime.max())
 plt.ticklabel_format(axis='y',style='sci',scilimits=(2,2))
 plt.bar(days,averageChoiceTime,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta4TiempoPorDia.png")
 
 plt.title("Personajes por tipo")
 colors = [{t<=characterTypes.min()*1.15: 'red',characterTypes.min()*1.15 <t<=characterTypes.max()/1.25: 'orange', t>characterTypes.max()/1.25: 'green'}[True] for t in characterTypes ]
@@ -142,7 +144,7 @@ plt.xlabel("Tipo de personaje")
 plt.ylabel("Número de personajes generados")
 plt.xticks(np.arange(0,len(characterTypes)))
 plt.bar(np.arange(0,len(characterTypes)),characterTypes,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta5TiposPersonaje.png")
 
 charCounter = 0
 for char in characterSentences:
@@ -152,7 +154,7 @@ for char in characterSentences:
       plt.ylabel("Número de apariciones")
       plt.xticks(np.arange(0,len(char)))
       plt.bar(np.arange(0,len(char)),char,color=colors)
-      plt.show()
+      plt.savefig(folder_path + "/Pregunta5FrasesTipo" + str(charCounter) + ".png")
       charCounter+=1
 
 plt.title("Favores totales")
@@ -161,7 +163,7 @@ plt.xlabel("ID del favor")
 plt.ylabel("Número de apariciones")
 plt.xticks(numvars)
 plt.bar(numvars,totalFavorApearance,color=colors)
-plt.show()
+plt.savefig(folder_path + "/Pregunta6AparicionFavores.png")
 
 plt.title("Pecados Total")
 colors = [{t<=totalSinAppearance.min()*1.15: 'red',totalSinAppearance.min()*1.15 <t<=totalSinAppearance.max()/1.25: 'orange', t>totalSinAppearance.max()/1.25: 'green'}[True] for t in totalSinAppearance ]
@@ -169,6 +171,6 @@ plt.xlabel("ID del pecado")
 plt.ylabel("Número de apariciones")
 plt.xticks(numvars)
 plt.bar(numvars,totalSinAppearance,color=colors)
-plt.show()    
+plt.savefig(folder_path + "/Pregunta6AparicionPecados.png")
                   
                 
